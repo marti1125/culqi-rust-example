2806d93591a1cdb99a3bf8156cc164a3a680c132
Willy Aguirre <marti1125@gmail.com>
2017-05-05 00:28:03 -0500

    add culqi checkout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket_contrib;
+extern crate rocket;
+extern crate serde_json;
+
+#[macro_use] extern crate serde_derive;
+
+use std::path::{Path, PathBuf};
+use rocket_contrib::Template;
+use rocket_contrib::{JSON};
+use rocket::response::NamedFile;
+
+#[get("/<file..>", rank = 5)]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+#[error(404)]
+fn not_found() -> Template {
+    let context = ();
+    Template::render("404", &context)
+}
+
+#[get("/")]
+fn index() -> Template {
+    let context = ();
+    Template::render("index", &context)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+    .mount("/", routes![files, index])
+    .catch(errors![not_found])
+    .launch();
 }
