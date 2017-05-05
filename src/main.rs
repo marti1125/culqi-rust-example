4000b6878e7e569d01a09b14e11c3495dc103e28
Willy Aguirre <marti1125@gmail.com>
2017-05-05 08:19:32 -0500

    add method charge

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket_contrib;
@@ -12,6 +12,12 @@ use rocket_contrib::Template;
 use rocket_contrib::{JSON};
 use rocket::response::NamedFile;
 
+#[derive(Debug, FromForm)]
+struct NewCharge {
+    token: String,
+    installments: i32
+}
+
 #[get("/<file..>", rank = 5)]
 fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
@@ -23,6 +29,13 @@ fn not_found() -> Template {
     Template::render("404", &context)
 }
 
+#[post("/charge?<charge>")]
+fn charge(charge: NewCharge) -> JSON<String> {
+    println!("t {:?}", charge);
+    JSON(charge.token)
+}
+
+
 #[get("/")]
 fn index() -> Template {
     let context = ();
@@ -31,7 +44,7 @@ fn index() -> Template {
 
 fn main() {
     rocket::ignite()
-    .mount("/", routes![files, index])
+    .mount("/", routes![files, index, charge])
     .catch(errors![not_found])
     .launch();
 }
